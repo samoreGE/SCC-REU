36f11800daf512b14f6a7a2469fc6b6678e1e79b
theDrunkenManatee <samorege@hendrix.edu>
2020-07-29 11:25:45 -0500

    rubber ducking works!

--- Griff/AHP/aggTester.m
+++ Griff/AHP/aggTester.m
@@ -18,21 +18,21 @@ randNegOffScores = [];
 mutualNegOffScores = [];
 randPosOffScores = [];
 mutualPosOffScores = [];
-for y = 4:4
+for y = 0:maxMembers-1
     y
-    %[negOffTasks, negOffWorkers] = makeCast(roster,maxMembers, -y);
-    %debug = "doNeg TESTS"
-    %[randNegPairs, randNegRanks, mutualNegPairs, mutualNegRanks] = doTests(negOffTasks, negOffWorkers);
-    %debug = "randNeg TEST"
-    %[randNegOffScores(y+1, 1), randNegOffScores(y+1, 2)] = scorePairs(roster, randNegPairs, randNegRanks, -y);
-    %debug = "mutualNeg TEST"
-    %[mutualNegOffScores(y+1, 1), mutualNegOffScores(y+1, 2)] = scorePairs(roster, mutualNegPairs, mutualNegRanks, -y);
+    [negOffTasks, negOffWorkers] = makeCast(roster,maxMembers, -y);
+    debug = "doNeg TESTS"
+    [randNegPairs, randNegRanks, mutualNegPairs, mutualNegRanks] = doTests(negOffTasks, negOffWorkers);
+    debug = "randNeg TEST"
+    [randNegOffScores(y+1, 1), randNegOffScores(y+1, 2)] = scorePairs(roster, randNegPairs, randNegRanks, -y);
+    debug = "mutualNeg TEST"
+    [mutualNegOffScores(y+1, 1), mutualNegOffScores(y+1, 2)] = scorePairs(roster, mutualNegPairs, mutualNegRanks, -y);
     
     [posOffTasks, posOffWorkers] = makeCast(roster,maxMembers, y);
     debug = "doPos TESTS"
     [randPosPairs, randPosRanks ,mutualPosPairs, mutualPosRanks] = doTests(posOffTasks, posOffWorkers);
-    %debug = "randPos SCORE"
-    %[randPosOffScores(y+1, 1), randPosOffScores(y+1, 2)] = scorePairs(roster, randPosPairs, randPosRanks, y);
+    debug = "randPos SCORE"
+    [randPosOffScores(y+1, 1), randPosOffScores(y+1, 2)] = scorePairs(roster, randPosPairs, randPosRanks, y);
     debug = "mutualPos SCORE"
     [mutualPosOffScores(y+1, 1), mutualPosOffScores(y+1, 2)] = scorePairs(roster, mutualPosPairs, mutualPosRanks, y);
 end
@@ -43,7 +43,6 @@ mutualPosOffScores
 end
 
 function [randomPairs, randomRanks, mutualPairs, mutualRanks] = doTests(tasks, workers)
-randomPairs = 0
-randomRanks = 0 %randomMatcher(tasks, workers);
+[randomPairs, randomRanks] = randomMatcher(tasks, workers);
 [mutualPairs, mutualRanks] = mutualMatcher(tasks, workers);
 end
